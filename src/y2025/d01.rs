2c2ee18fc5ece138751570da2834bd9039632c1e
qerdcv <vadym.tishchenko2@gmail.com>
2025-12-01 08:03:08 +0200

    Solve y2025d01

--- src/y2025/d01.rs
+++ src/y2025/d01.rs
@@ -1,7 +1,65 @@
+const DIAL_POSITIONS: i64 = 100;
+
 pub fn p1(input: &str) -> i64 {
-    0
+    let mut dial = 50;
+    let mut result = 0;
+    for (dir, times) in input
+        .lines()
+        .filter(|l| !l.is_empty())
+        .map(|l| {
+            let l = l.as_bytes();
+            (l[0], str::from_utf8(&l[1..]).unwrap().parse::<i64>().unwrap())
+        }) {
+        dial = match dir {
+            b'R' => dial + times,
+            b'L' => dial - times,
+            _ => panic!("Unexpected direction"),
+        }.rem_euclid(DIAL_POSITIONS);
+
+        if dial == 0{
+            result += 1;
+        }
+    }
+
+    result
 }
 
 pub fn p2(input: &str) -> i64 {
-    0
+    let mut dial = 50;
+    let mut result = 0;
+    for (dir, times) in input
+        .lines()
+        .filter(|l| !l.is_empty())
+        .map(|l| {
+            let l = l.as_bytes();
+            (l[0], str::from_utf8(&l[1..]).unwrap().parse::<i64>().unwrap())
+        }) {
+        result += match dir {
+            b'R' => {
+                let h = if dial == 0 { DIAL_POSITIONS } else { DIAL_POSITIONS - dial };
+                if times < h {
+                    0
+                } else {
+                    1 + (times - h) / DIAL_POSITIONS
+                }
+            }
+            b'L' => {
+                let h = if dial == 0 { DIAL_POSITIONS } else { dial };
+                if times < h {
+                    0
+                } else {
+                    1 + (times - h) / DIAL_POSITIONS
+                }
+            }
+            _ => panic!("Unexpected direction"),
+        };
+
+        dial = match dir {
+            b'R' => dial + times,
+            b'L' => dial - times,
+            _ => panic!("Unexpected direction"),
+        }.rem_euclid(DIAL_POSITIONS);
+    }
+
+    result
 }
