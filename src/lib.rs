3ea7a7eeb4aa2a4393895a3806aca5697216ed9d
qerdcv <vadym.tishchenko2@gmail.com>
2025-12-03 22:58:03 +0200

    Add lookup tables for other years

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ pub mod y2025;
 pub fn lookup(y: usize, d: usize) -> (fn(&str) -> i64, fn(&str) -> i64){
     let d = d - 1;
     match y {
+        2015 => y2015::LOOKUP[d],
+        2022 => y2022::LOOKUP[d],
         2025 => y2025::LOOKUP[d],
         _ => unimplemented!(),
     }
--- src/y2022/mod.rs
+++ src/y2022/mod.rs
@@ -10,3 +10,18 @@ pub mod d09;
 pub mod d10;
 pub mod d11;
 pub mod d12;
+
+pub static LOOKUP: [(fn(&str) -> i64, fn(&str) -> i64); 12] = [
+    (d01::p1, d01::p2),
+    (d02::p1, d02::p2),
+    (d03::p1, d03::p2),
+    (d04::p1, d04::p2),
+    (d05::p1, d05::p2),
+    (d06::p1, d06::p2),
+    (d07::p1, d07::p2),
+    (d08::p1, d08::p2),
+    (d09::p1, d09::p2),
+    (d10::p1, d10::p2),
+    (d11::p1, d11::p2),
+    (d12::p1, d12::p2),
+];
